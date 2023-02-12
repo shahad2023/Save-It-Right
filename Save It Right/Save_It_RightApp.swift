4c596a35d9165aeb5f3a66ecd017917ed56ece64
Shoug <Shoug.turki1@gmail.com>
2023-02-12 10:03:19 +0300

    Initial Commit

---
+++ Save It Right/Save_It_RightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Save_It_RightApp.swift
+//  Save It Right
+//
+//  Created by Shouq Turki Bin Tuwaym on 12/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Save_It_RightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
